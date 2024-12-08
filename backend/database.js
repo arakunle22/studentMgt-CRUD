51005e92a6df93ed7903c5805ec1898ec5366bb1
arakunle22 <103513961+arakunle22@users.noreply.github.com>
2024-12-08 10:25:48 +0100

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false }, // This is required if you're using a self-signed certificate
+  connectionString: isProduction
+    ? process.env.DB_URL_INTERNAL // Use internal URL in production
+    : process.env.DB_URL_EXTERNAL, // Use external URL locally
+  ssl: isProduction ? false : { rejectUnauthorized: false }, // Only use SSL for external connections
 });
 
 module.exports = pool;
+
+
 
